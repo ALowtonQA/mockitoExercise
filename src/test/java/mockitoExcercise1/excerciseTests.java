6444fda7228f0be223fe46ca913cfe250704b65e
ALowton <ALowton@QA.com>
2021-08-23 15:02:54 +0100

    AL: Added solutions and support for annotations

--- src/test/java/mockitoExcercise1/excerciseTests.java
+++ src/test/java/mockitoExcercise1/excerciseTests.java
@@ -1,26 +1,56 @@
 package mockitoExcercise1;
 
-import static org.junit.Assert.*;
 import static org.mockito.Matchers.anyInt;
 import static org.mockito.Mockito.doThrow;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
 
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 
+@RunWith(MockitoJUnitRunner.class)
 public class excerciseTests {
+	
+	@Mock
+	Printer printerMock;
+	
+	@Mock
+	NumberCalculator numberCalculatorMock;
+	
+	@InjectMocks
+	excercise1 ex1;
+	
     @Test
     public void printsCalculatorResultsHundredTimes() {
-    	fail("Not yet implemented");
+    	String limit = "100";
+        
+        when(numberCalculatorMock.calculate(anyInt()))
+                .thenReturn("0", "1");
+
+        ex1.printNumbers(100);
+        
+        verify(numberCalculatorMock, times(100)).calculate(anyInt());
+        verify(printerMock, times(1)).print("0");
+        verify(printerMock, times(Integer.parseInt(limit) - 1)).print("1");
     }
  
     @Test
     public void continuesOnCalculatorOrPrinterError() {
-        fail("Not yet implemented");
+        when(numberCalculatorMock.calculate(anyInt()))
+                .thenReturn("1")
+                .thenThrow(new RuntimeException())
+                .thenReturn("3");
+        
+        doThrow(new RuntimeException()).when(printerMock).print("3");
+        
+        ex1.printNumbers(3);
+        
+        verify(numberCalculatorMock, times(3)).calculate(anyInt());
+        verify(printerMock).print("1");
+        verify(printerMock).print("3");
     }
 }
